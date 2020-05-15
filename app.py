c9587a7565b0ab622ecf0e4606b9ba26a9d999d5
Jim Collins <JCollins@compusearch.com>
2020-05-15 19:03:41 -0400

    modified app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from models.tradeking import TradeKingAPI
 from markupsafe import escape
+import json
 import os
 """
 https://github.com/public-apis/public-apis/blob/145ea881b2b15034d7207f7f7b6a781994fbb574/README.md#cryptocurrency
@@ -20,6 +21,7 @@ use relative paths
 
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
@@ -59,8 +61,14 @@ def display_clock():
 def display_quote(symb):
     print('symb: ', symb, flush=True)
     print('symb: ', escape(symb), flush=True)
+    fids = 'last,bid,ask'
     tk = TradeKingAPI()
-    ext_quotes_data = tk.market_ext_quotes(escape(symb),)
+    ext_quotes_data = tk.market_ext_quotes(escape(symb), fids)
+    quote_last = ext_quotes_data.json()
+    print('response last: ', quote_last['response']['quotes']['quote']['last'])
+    for keys, values in quote_last['response']['quotes']['quote'].items():
+        print('keys: ', keys)
+        print('values: ', values)
     return render_template('DisplayBTCData.html', btcdata=ext_quotes_data.json())
 
 
@@ -68,6 +76,7 @@ def display_quote(symb):
 def display_options(sym):
     tk = TradeKingAPI()
     fids = 'ask,bid,vol,strikeprice'
+    # def market_options_search(self, symbol, query=None, fids=None):
     options_data = tk.market_options_search(symbol=sym, fids=fids)
     print('options url: ', options_data.url)
     return render_template('DisplayBTCData.html', btcdata=options_data.json())
@@ -76,17 +85,33 @@ def display_options(sym):
 @app.route('/options_strikes/<sym>')
 def display_options_strikes(sym):
     tk = TradeKingAPI()
-    fids='ask,bid,vol,strikeprice'
+    # def market_options_strikes(self, symbol):
     options_strikes = tk.market_options_strikes(symbol=sym)
-    print('options_strikes url: ', options_strikes.url)
-    return render_template('DisplayBTCData.html', btcdata=options_strikes.json())
+    strikes = options_strikes.json()
+    print('strikes:', strikes)
+    strikelist = strikes['response']
+    strikelist = strikelist['prices']
+    print('strikelist:', strikelist)
+    print('options_strikes url: ', options_strikes.url, flush=True)
+    print('options_strikes json: ', strikelist, flush=True)
+    print('data type:', type(strikelist), flush=True)
+    list_values = [v for v in strikelist.values()]
+    list_values = [x for x in list_values]
+    print('data type-list:', type(list_values), flush=True)
+    print('list values: ', list_values, flush=True)
+    for x in list_values:
+        for y in x:
+            print('y:', y)
+    return render_template('DisplayBTCData.html', btcdata=strikelist)
 
 
 @app.route('/options_expire/<sym>')
 def display_options_expirations(sym):
     tk = TradeKingAPI()
+    # def market_options_expirations(self, symbol):
     options_expire = tk.market_options_expirations(sym)
-    return render_template('DisplayBTCData.html', btcdata=options_expire)
+    print('options_expire url: ', options_expire.url)
+    return render_template('DisplayBTCData.html', btcdata=options_expire.json())
 
 
 if __name__ == '__main__':
